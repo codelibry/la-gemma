08356bbce4070374b75862abfbc52a562c4f0e44
modifikult <dev@flooow.agency>
2023-07-20 17:19:52 +0300

    add text image adaptive for default template

--- template-parts/acf-block/text_gallery.php
+++ template-parts/acf-block/text_gallery.php
@@ -10,7 +10,7 @@
     <section class="text-gallery">
         <div class="container">
             <div class="text-gallery__wrap d-flex align-items-center justify-content-center justify-content-lg-between">
-                <div class="text-gallery__slider js-slider <?php echo $gallery_position === 'left' ? 'order-1 gallery-left' : 'order-1 order-lg-2 gallery-right'; ?>">
+                <div class="text-gallery__slider js-slider desktop <?php echo $gallery_position === 'left' ? 'order-1 gallery-left' : 'order-1 order-lg-2 gallery-right'; ?>">
                     <?php foreach ($gallery as $image) : ?>
                         <div class="text-gallery__slider-item">
                             <img src="<?php echo $image['url']; ?>"
@@ -22,6 +22,14 @@
                     <div class="text-gallery__content <?php echo $gallery_position === 'left' ? 'order-2 content-right' : 'order-2 order-lg-1 content-left'; ?>">
                         <div class="text-gallery__title">
                             <?php echo $title; ?>
+                        </div>
+                        <div class="text-gallery__slider js-slider mobile <?php echo $gallery_position === 'left' ? 'order-1 gallery-left' : 'order-1 order-lg-2 gallery-right'; ?>">
+                            <?php foreach ($gallery as $image) : ?>
+                                <div class="text-gallery__slider-item">
+                                    <img src="<?php echo $image['url']; ?>"
+                                         alt="<?php echo $image['alt'] ?: $image['title']; ?>">
+                                </div>
+                            <?php endforeach; ?>
                         </div>
                         <div class="text-gallery__text">
                             <?php echo $text; ?>
--- template-parts/acf-block/text_image.php
+++ template-parts/acf-block/text_image.php
@@ -9,14 +9,14 @@
     <section class="text-image">
         <div class="container">
             <div class="row d-flex justify-content-center justify-content-lg-between text-image__wrap">
-                <div class="col-12 col-lg-6 text-image__img <?php echo $image_position === 'left' ? 'order-1 image-left' : 'order-1 order-lg-2 image-right'; ?>">
+                <div class="col-12 col-lg-6 text-image__img <?php echo $image_position === 'left' ? 'order-2 order-lg-1 image-left' : 'order-2 image-right'; ?>">
                     <div class="text-image__img-wrap">
                         <img src="<?php echo $image['url']; ?>"
                              alt="<?php echo $image['alt'] ?: $image['title']; ?>">
                     </div>
                 </div>
                 <?php if($title || $text) :?>
-                    <div class="col-12 col-lg-6 text-center text-image__content <?php echo $image_position === 'left' ? 'order-2 content-right' : 'order-2 order-lg-1 content-left'; ?>">
+                    <div class="col-12 col-lg-6 text-center text-image__content <?php echo $image_position === 'left' ? 'order-1 order-lg-2 content-right' : 'order-1 content-left'; ?>">
                         <?php if($title) :?>
                             <div class="text-image__title">
                                 <?php echo $title;?>
