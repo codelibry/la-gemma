5ddf7bfa8635e54f35fa268c6e95d24e690d01df
modifikult <dev@flooow.agency>
2023-07-25 13:40:56 +0300

    fix the reservation form number bug

--- template-parts/acf-block/reservation_form.php
+++ template-parts/acf-block/reservation_form.php
@@ -6,7 +6,10 @@
             </div>
             <div class="form-text">
                 <?php _e('We accept reservations online for up to 6 people, for parties of 7 or more please call us on', 'lagemmahotel'); ?>
-                <span class="phone"><a href="tel:+38 (000) 000 00 00">+38 (000) 000 00 00</a></span>
+                <span class="phone">
+                    <span>***********</span>
+                    <a href="tel:+38 (000) 000 00 00">+38 (000) 000 00 00</a>
+                </span>
             </div>
             <div class="form-submit">
                 <div id="sr-res-root" class="button js-submit"><?php _e('Book Now', 'lagemmahotel'); ?></div>
