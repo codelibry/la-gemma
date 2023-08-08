3ecd9478a78e6c411116fad5e3d5e555f77c178c
modifikult <dev@flooow.agency>
2023-08-08 16:01:51 +0300

    more fixes
    - added newsletter section for all pages
    - change margins and paddings between footer and newsletter blocks
    - fixed the size of the menu button and the "Book now" button so that they are the same size

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 		
 		</main>
         
-        <?php if(!is_front_page()) :?>
-            <?php get_template_part('template-parts/blocks/newsletter'); ?>
-        <?php endif; ?>
+        <?php get_template_part('template-parts/blocks/newsletter'); ?>
         
         <?php get_template_part('template-parts/footer/footer'); ?>
 
--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -1,60 +1,58 @@
 <?php
-    $footer_logo_light = get_field('footer_logo_light', 'option');
-    $footer_logo_dark = get_field('footer_logo_dark', 'option');
+//    $footer_logo_light = get_field('footer_logo_light', 'option');
+//    $footer_logo_dark = get_field('footer_logo_dark', 'option');
     $footer_info = get_field('footer_info', 'option');
     $footer_address = get_field('footer_address', 'option');
     
-    if(!is_page_template('template-pages/lucas-page.php') && !is_page_template('template-pages/paulo-airaudo-page.php')) {
-        $footer_logo = $footer_logo_light;
-    } else {
-        $footer_logo = $footer_logo_dark;
-    }
+//    if (!is_page_template('template-pages/lucas-page.php') && !is_page_template('template-pages/paulo-airaudo-page.php')) {
+//        $footer_logo = $footer_logo_light;
+//    } else {
+//        $footer_logo = $footer_logo_dark;
+//    }
 ?>
 
-<?php if(!is_front_page()) :?>
-    <footer class="footer">
-        <div class="container">
-            <div class="footer__wrap">
-                <?php if ($footer_logo) : ?>
-                    <div class="footer__logo">
-                        <img src="<?php echo $footer_logo['url']; ?>"
-                             alt="<?php echo $footer_logo['alt'] ?: $footer_logo['title']; ?>">
-                    </div>
-                <?php endif; ?>
-                
-                <?php if ($footer_info) : ?>
-                    <div class="footer__info">
-                        <ul class="info__list flex-wrap">
-                            <?php foreach ($footer_info as $info_item) : ?>
-                                <?php $info = $info_item['info_item']; ?>
-                                <li class="info__list-item">
-                                    <a href="<?php echo preg_match("/@|\.com/", $info) ? 'mailto:' . $info : 'tel:' . $info; ?>">
-                                        <?php echo $info; ?>
-                                    </a>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                
-                <?php endif; ?>
-                
-                <?php if ($footer_address) : ?>
-                    <div class="footer__address">
-                        <?php echo $footer_address; ?>
-                    </div>
-                <?php endif; ?>
-            </div>
+<footer class="footer">
+    <div class="container">
+        <div class="footer__wrap">
+<!--            --><?php //if ($footer_logo) : ?>
+<!--                <div class="footer__logo">-->
+<!--                    <img src="--><?php //echo $footer_logo['url']; ?><!--"-->
+<!--                         alt="--><?php //echo $footer_logo['alt'] ?: $footer_logo['title']; ?><!--">-->
+<!--                </div>-->
+<!--            --><?php //endif; ?>
+            
+            <?php if ($footer_info) : ?>
+                <div class="footer__info">
+                    <ul class="info__list flex-wrap">
+                        <?php foreach ($footer_info as $info_item) : ?>
+                            <?php $info = $info_item['info_item']; ?>
+                            <li class="info__list-item">
+                                <a href="<?php echo preg_match("/@|\.com/", $info) ? 'mailto:' . $info : 'tel:' . $info; ?>">
+                                    <?php echo $info; ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            
+            <?php endif; ?>
+            
+            <?php if ($footer_address) : ?>
+                <div class="footer__address">
+                    <?php echo $footer_address; ?>
+                </div>
+            <?php endif; ?>
         </div>
-    </footer>
-<?php endif; ?>
-
-<?php if(is_front_page()) :?>
-    <div class="story-btn">
-        <a href="#123">
-            <div class="story-btn__icon">
-                <img src="<?php echo get_template_directory_uri() . '/assets/image/story-btn-icon.png';?>" alt="story-btn">
-            </div>
-            <div class="story-btn__text"><?php _e('Story');?></div>
-        </a>
     </div>
-<?php endif; ?>
+</footer>
+
+<?php //if(is_front_page()) :?>
+<!--    <div class="story-btn">-->
+<!--        <a href="#123">-->
+<!--            <div class="story-btn__icon">-->
+<!--                <img src="--><?php //echo get_template_directory_uri() . '/assets/image/story-btn-icon.png';?><!--" alt="story-btn">-->
+<!--            </div>-->
+<!--            <div class="story-btn__text">--><?php //_e('Story');?><!--</div>-->
+<!--        </a>-->
+<!--    </div>-->
+<?php //endif; ?>
